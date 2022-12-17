e2537970cd94890d093e00b71666794990a55e74
Du <hoangminhdu2000@gmail.com>
2022-12-17 11:15:48 +0700

    update section 7 & section 8

--- src/components/Section7.tsx
+++ src/components/Section7.tsx
@@ -3,7 +3,7 @@ export default function Section7() {
     <div className="relative bg-gray-50 pt-12 sm:pt-16">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl font-extrabold text-red-500 sm:text-4xl">
             What's next?
           </h2>
 
--- src/components/Section8.tsx
+++ src/components/Section8.tsx
@@ -3,11 +3,11 @@ export default function Section7() {
     <div className="relative bg-gray-50 pt-12 sm:pt-16 pb-10">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl mb-3">
+          <h2 className="text-3xl font-extrabold text-red-500 sm:text-4xl mb-3">
             KompetitorS
           </h2>
 
-          <img src="/assets/section8/section8.png" />
+          <img src="/assets/section8/section8_1.png" />
         </div>
       </div>
     </div>
